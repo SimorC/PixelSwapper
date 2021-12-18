57f91e378987af3a9bdf4bfd13715099e64e06f6
Rafael Simor <rafaelsimor@gmail.com>
2021-12-18 17:00:26 +0000

    add sample images + appsettings

--- PixelSwapper.App/Program.cs
+++ PixelSwapper.App/Program.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 
 namespace PixelSwapper.App
 {
     class Program
     {
+        private static IConfigurationRoot _config;
+
+        public static void SetupConfig()
+        {
+            var builder = new ConfigurationBuilder()
+               .AddJsonFile($"appsettings.json", true, true);
+
+            _config = builder.Build();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SetupConfig();
+
+            var defaultFileName = _config["DefaultFileName"];
+            Console.WriteLine($"Press enter to process 'in/{defaultFileName}'...");
             Console.ReadKey();
+
+
         }
     }
 }
